2131507ea8e85ef778e67f3c3a971de86e3c2aa3
Emre Kaskaval <emre.kaskaval@live.nl>
2018-09-25 21:17:27 +0300

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
     <div class="container has-text-centered">
       <h1 class="title">About Me</h1>
       <p>
-        My name is Emre Kaskaval (helldoradono), I’m a self taught software developer based in Lagos, Nigeria. I build modern applications for the web! I'm a technical writer, I write technical articles and tutorials for various platforms including <a href="#">Scotch.io</a>.
+        My name is Emre Kaskaval (helldoradono), I’m a self taught software developer based in Istanbul, Turkey. I build modern applications for the web! I'm a technical writer, I write technical articles and tutorials for various platforms including <a href="#">Scotch.io</a>.
       </p>
     </div>
   </section>
